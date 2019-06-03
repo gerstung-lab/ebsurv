96d82281e7d23a44945cb5bdb38b82c4d3b44d38
Rui Costa <ruibarrigana@ebi.ac.uk>
2019-06-03 14:10:28 +0100

    Ancillary function for boot_probtrans

--- R/bootstrap_CIs.R
+++ R/bootstrap_CIs.R
@@ -50,3 +50,62 @@ boot_coxrfx<-function(mstate_data_expanded,which_group,min_nr_samples=100,output
     return(CIs)
   }
 }
+
+
+#' Bootstrap confidence intervals for transition probabilities
+#' 
+#' Generates interval estimates for transition probabilities computed using \code{probtrans_ebsurv} (semi-Markov version).
+#' 
+#' @param coxrfx_fits_boot The list of CoxRFX objects obtained by running \code{boot_coxrfx}.
+#' @param patient_data (Single) patient data in `long format`, possibly with `expanded` covariates
+#' (as obtained by running mstate::expand.covs).
+#' @param tmat Transition matrix.
+#' @return Interval estimates for transition probabilities. 
+#' @author rc28
+#' @export
+
+boot_probtrans<-function(coxrfx_fits_boot,patient_data,tmat){
+  msfit_objects_boot<-vector("list",length(coxrfx_fits_boot))
+  probtrans_objects_boot<-vector("list",length(coxrfx_fits_boot))
+  for(i in 1:length(coxrfx_fits_boot)){
+    print(i)
+    covariate_df<-as.data.frame(coxrfx_fits_boot[[i]]$Z)
+    covariate_df$transition<-coxrfx_fits_boot[[i]]$transition
+    mstate.data.expanded.boot<-list()
+    mstate.data.expanded.boot$time<-coxrfx_fits_boot[[i]]$surv[,1]
+    mstate.data.expanded.boot$status<-coxrfx_fits_boot[[i]]$surv[,2]
+    patient_data2<-patient_data[names(patient_data)%in%names(covariate_df)]
+    patient_data2$strata<-patient_data$strata
+    
+    environment(coxrfx_fits_boot[[1]]$formula)$covariate_df<-covariate_df
+    
+    msfit_objects_boot[[i]]<-msfit_generic(coxrfx_fits_boot[[i]],patient_data2,trans=tmat)
+    probtrans_objects_boot[[i]]<-probtrans_ebsurv(msfit_objects_boot[[i]],"semiMarkov")[[1]]
+    probtrans_objects_boot[[i]]<-probtrans_objects_boot[[i]][sapply(seq(from=0,to=max(probtrans_objects_boot[[i]]$time),length.out = 400),function(x) which.min(abs(probtrans_objects_boot[[i]]$time-x))),]
+    
+  }
+
+  probtrans_boot_CIs_for_target_state<-function(target_state){
+    target_state_boot_samples<-as.data.frame(sapply(probtrans_objects_boot, extract_function,item=target_state))
+    apply(target_state_boot_samples,1,HDInterval::hdi,credMass=0.95)
+  }
+  probtrans_CIs<-lapply(colnames(tmat),probtrans_boot_CIs_for_target_state)
+  names(probtrans_CIs)<-colnames(tmat)
+  return(probtrans_CIs)
+}
+
+#' Return all the bootstrap transition probabilities for target state `item` from a list with bootstrap transition probabilities for multiple states.
+#' 
+#' Ancillary function of \code{boot_probtrans}, not meant to be called by the user.
+#' 
+#' @param list_object
+#' @param item
+#' @return Bootstrap transition probabilities for target state `item`. 
+#' @author rc28
+#' @export
+
+extract_function<-function(list_object,item){
+  as.vector(list_object[item])
+}
+
+
