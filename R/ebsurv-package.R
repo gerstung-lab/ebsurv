2c8f8dd1761ee0980c28ec7987d286c6a5448e99
Rui Costa <ruibarrigana@ebi.ac.uk>
2020-01-21 16:37:51 +0000

    Fixing the package help page.

--- R/ebsurv-package.R
+++ R/ebsurv-package.R
@@ -1,7 +1,7 @@
 #'Empirical Bayes multi-state Cox model
 #' 
 #'This package implements an empirical Bayes,
-#'multi-state Cox model.Different groups of regression
+#'multi-state Cox model. Different groups of regression
 #'coefficients can be defined, with coefficients of the
 #'same group sharing the same Gaussian prior. It takes
 #'as input a data set in 'long format' and generates
@@ -11,6 +11,14 @@
 #' @name ebsurv-package
 #' @docType package
 #' @author Rui Costa, Moritz Gerstung
+#' @details
+#' \tabular{ll}{
+#' Package: \tab ebsurv\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.0.73\cr
+#' Date: \tab 2020-01-21\cr
+#' License: \tab GPL 3\cr
+#' }
 #' @keywords package
 #' @import parallel
 #' @import survival
